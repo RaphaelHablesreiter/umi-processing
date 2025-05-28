5e411cd5e0a52ad7312f35bc3acaf609292edde9
Hannah A. Leptin <168175482+hannahlept0n@users.noreply.github.com>
2025-05-28 13:25:20 +0200

    Update AddParameters.R
    Adjustments to remove collums "EBScore" and "Multiallelic" from variantcalls.csv. Note that "umi-variantcalling/rules/annotate.smk" was also adjusted in this context.

--- umi-variantcalling/scripts/AddParameters.R
+++ umi-variantcalling/scripts/AddParameters.R
@@ -25,34 +25,35 @@ fisher <- round(dhyper(as.numeric(input["TR1_plus"][[1]]),
                 digits = 4) * -1
 input["FisherScore"] <- fisher
 
-input["MultiAllelic"] <- 0
-input["EBScore"] <- 0
+# uncomment to add collums "EBScore" and "MultiAllelic", note that "umi-variantcalling/rules/annotate.smk" has to be adjusted too
+#input["MultiAllelic"] <- 0
+#input["EBScore"] <- 0snake
 
-if (nrow(input) != length(eb))
-{
-    y <- which(colnames(input) == "MultiAllelic")
-    z <- which(colnames(input) == "EBScore")
-    input[1,z] <- eb[1]
-    j = 2
-    for (i in 2:nrow(input))
-    {
-        if (all(input[i-1,c(1:3)] == input[i,c(1:3)]))
-        {
-            input[i,z] <- eb[j]
-            input[i-1,y] <- 1
-            input[i,y] <- 1
+#if (nrow(input) != length(eb))
+#{
+#    y <- which(colnames(input) == "MultiAllelic")
+#    z <- which(colnames(input) == "EBScore")
+#    input[1,z] <- eb[1]
+#    j = 2
+    # for (i in 2:nrow(input))
+    # {
+    #     if (all(input[i-1,c(1:3)] == input[i,c(1:3)]))
+    #     {
+    #         input[i,z] <- eb[j]
+    #         input[i-1,y] <- 1
+    #         input[i,y] <- 1
             
-        } else {
-            input[i,z] <- eb[j]
-            j = j + 1
-        }
-    }
+    #     } else {
+    #         input[i,z] <- eb[j]
+    #         j = j + 1
+    #     }
+#    }
     
     
-} else {
-    input["EBScore"] <- eb
-    
-}
+#} else {
+#    input["EBScore"] <- eb
+#    
+#}
 
 # Calculate VAF
 input["TVAF"] <- round(as.numeric(input["TR2"][[1]])/
